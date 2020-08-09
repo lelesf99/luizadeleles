73855e6a3d0d19a563317227ede0c0d8be1f5402
lelesf99 <41308473+lelesf99@users.noreply.github.com>
2020-08-09 02:46:05 -0300

    first

---
+++ public/js/events.js
@@ -0,0 +1,36 @@
+var navClusters = document.querySelectorAll('.navCluster');
+var backNavs = document.querySelectorAll('.backNav');
+
+window.onhashchange = function(){
+	updateContent();
+}
+window.onload = function(){
+	updateContent();
+}
+document.querySelector("#togglemenu").addEventListener("click", ()=>{
+	document.querySelector("#navbar").classList.toggle('collapsed');
+});
+document.querySelectorAll('.singleNav').forEach(function(element){
+	element.addEventListener('click', (event)=>{
+		window.location.hash = element.text;
+		stopDefAction(event);
+	});
+});
+for (let i = 0; i < navClusters.length; i++) {
+	let backNav = navClusters[i].firstElementChild;
+	let nav = navClusters[i].lastElementChild;
+	nav.addEventListener('click', (event)=>{
+		console.log(nav.classList.contains('disabled'));
+		if(nav.classList.contains('disabled')){
+			stopDefAction(event);
+		}else{
+			window.location.hash = nav.getAttribute('href');
+			stopDefAction(event);
+			clearNavs();
+		}
+	});
+	backNav.addEventListener('click', (event)=>{
+		window.location.hash = '#Galeria';
+		stopDefAction(event);
+	});
+}
---
+++ public/js/frontEnd.js
@@ -0,0 +1,73 @@
+var navClusters = document.querySelectorAll('.navCluster');
+var backNavs = document.querySelectorAll('.backNav');
+
+function stopDefAction(evt) {
+    evt.preventDefault();
+}
+function loadHomePage(){
+	console.log("HomeLoad");
+	document.querySelector("#imgs").classList.remove('on');
+	document.querySelector("#about").classList.remove('on');
+	document.querySelector(".gallery").classList.remove('on');
+	document.querySelector(".homePage").classList.add('on');
+}
+function loadGalleryPage(){
+	console.log("galLoad");
+	document.querySelector("#about").classList.remove('on');
+	document.querySelector(".homePage").classList.remove('on');
+	document.querySelector(".gallery").classList.add('on');
+	document.querySelector("#imgs").classList.add('on');
+}
+function loadAboutPage(){
+	console.log("aboutLoad");
+	document.querySelector(".homePage").classList.remove('on');
+	document.querySelector("#imgs").classList.remove('on');
+	document.querySelector(".gallery").classList.add('on');
+	document.querySelector("#about").classList.add('on');
+}
+function clearNavs(){
+	for (let i = 0; i < navClusters.length; i++) {
+		let backNav = navClusters[i].firstElementChild;
+		let nav = navClusters[i].lastElementChild;
+		backNav.classList.add('backNavToggle');
+		nav.classList.remove('disabled');
+	}
+}
+function updateContent(){
+	page = window.location.hash.replace(/\#/, '');
+	clearNavs();
+	if(page === "Home" || page === ""){
+		loadHomePage();
+
+	}else if(page === "Galeria"){
+		loadGalleryPage();
+		
+		document.querySelectorAll(".img").forEach(function(element){
+			element.classList.remove('hid');
+		});
+	}else if(page === "Sobre"){
+		loadAboutPage();
+		document.querySelector("#backSobre").classList.remove('backNavToggle');
+		document.querySelector("#SobreNav").classList.add('disabled');
+	}else{
+		document.querySelectorAll(".img").forEach(function(element){
+			if(!element.classList.contains(page)){
+				element.classList.add('hid');
+			}else{
+				element.classList.remove('hid');
+			}
+			for (let i = 0; i < navClusters.length; i++) {
+				let backNav = navClusters[i].firstElementChild;
+				let nav = navClusters[i].lastElementChild;
+				if(nav.text === page){
+					backNav.classList.remove('backNavToggle');
+					nav.classList.add('disabled');
+				}
+			}
+		});
+		loadGalleryPage();
+	}
+	if(document.querySelector("#navbar").classList.contains('collapsed'))
+		document.querySelector("#navbar").classList.remove('collapsed');
+}
+
